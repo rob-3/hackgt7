f1ccf985369dec30fdaf4b475b44fe3493e56443
dylans123 <dylanskelly@knights.ucf.edu>
2020-10-16 22:18:33 -0400

    Initial work for the maps

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Map from './src/components/Map.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Map />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
---
+++ src/components/Map.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
+import MapView from 'react-native-maps';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialRegion: null,
+      found: false
+    };
+    this.goToInitialRegion = this.goToInitialRegion.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        let region = {
+          latitude: parseFloat(position.coords.latitude),
+          longitude: parseFloat(position.coords.longitude),
+          latitudeDelta: 5,
+          longitudeDelta: 5
+        };
+        this.setState({
+          initialRegion: region,
+          found: true
+        });
+        this.goToInitialRegion();
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+      }
+    );
+  }
+
+  goToInitialRegion() {
+    let initialRegion = Object.assign({}, this.state.initialRegion);
+    initialRegion['latitudeDelta'] = 0.1;
+    initialRegion['longitudeDelta'] = 0.1;
+    this.mapView.animateToRegion(initialRegion, 2000);
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.found ? (
+          <MapView
+            style={styles.mapStyle}
+            region={this.state.mapRegion}
+            followUserLocation={true}
+            ref={ref => (this.mapView = ref)}
+            zoomEnabled={true}
+            showsUserLocation={true}
+            onMapReady={this.goToInitialRegion}
+            initialRegion={this.state.initialRegion}
+          />
+        ) : <ActivityIndicator size="large" />}
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default Map;
