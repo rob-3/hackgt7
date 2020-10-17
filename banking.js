a60ff721fc4984428899c4e29b1101501de2e3eb
Fulton Garcia <fultongarcia@gmail.com>
2020-10-16 22:40:24 -0400

    banking initial endpoint working with axios

--- banking.js
+++ banking.js
@@ -1,27 +1,28 @@
+import axios from 'axios';
+import qs from 'qs';
 export const getAuthCode = () => {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  myHeaders.append("Authorization", "Basic alI3RWg3dUF5cFQ0dEpMb0xVMmRBTVlHQ1l5ejZsVjg6T3FRZXQ0OE5YWDdTQXB4SA==");
-  myHeaders.append("transactionId", "640c8294-f956-42ed-a22c-fe056f4d7981");
-  myHeaders.append("institutionId", "00516");
-  myHeaders.append("Accept", "application/json");
-
-  const urlencoded = new URLSearchParams();
-  urlencoded.append("grant_type", "client_credentials");
-  urlencoded.append("scopes", "accounts:read,transactions:read,transfers:write,account:write,institution-users:read,recipients:read,recipients:write,recipients:delete,disclosures:read,disclosures:write");
-
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: 'follow'
+  var data = qs.stringify({
+   'grant_type': 'client_credentials',
+  'scopes': 'accounts:read,transactions:read,transfers:write,account:write,institution-users:read,recipients:read,recipients:write,recipients:delete,disclosures:read,disclosures:write' 
+  });
+  var config = {
+    method: 'post',
+    url: 'http://ncrdev-dev.apigee.net/digitalbanking/oauth2/v1/token',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': 'Basic alI3RWg3dUF5cFQ0dEpMb0xVMmRBTVlHQ1l5ejZsVjg6T3FRZXQ0OE5YWDdTQXB4SA==', 
+      'transactionId': '0cd335cd-58ce-459c-baba-0c0a638d6783', 
+      'institutionId': '00516', 
+      'Accept': 'application/json'
+    },
+    data : data
   };
-
-  return fetch("http://ncrdev-dev.apigee.net/digitalbanking/oauth2/v1/token", requestOptions)
-    .then(response => {
-      console.log(response.status)
-      return response.text()
-    })
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
